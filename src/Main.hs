77ccf0a2bd925e4f6b896b0ae2db667156858dd4
QuentinDuval <senmenty@gmail.com>
2015-11-29 03:17:00 +0100

    Trying criterion for the bench of queues

--- src/Main.hs
+++ src/Main.hs
@@ -2,21 +2,34 @@ module Main (
     main
 ) where
 
+import Criterion
+import Criterion.Main
+
 import Queue.Class
 import qualified Queue.Persistent as Persistent
 import qualified Queue.Transient as Transient
 import Queue.Utils
 
+import System.IO
+
 
 main :: IO ()
 main = do
-    testQueue Transient.create
-    testQueue Persistent.create
+    -- Settings for better output by criterion
+    hSetBuffering stdout NoBuffering
+    hSetEncoding stdout utf8
+
+    -- Test cases for criterion
+    defaultMain [
+        bgroup "Queue" [
+            bench "Transient"  $ nf testQueue Transient.create  ,
+            bench "Persistent" $ nf testQueue Persistent.create ]
+        ]
 
 
-
-testQueue :: (IQueue q) => ([Int] -> q Int) -> IO ()
-testQueue create = do
+testQueue :: (IQueue q) => ([Int] -> q Int) -> [Int]
+testQueue create =
     let q1 = push 12 $ push 11 $ pop $ create [0..10 :: Int]
-    let q2 = foldl (flip push) q1 [13 .. 100 :: Int]
-    print $ consume (:) [] q2
+        q2 = foldl (flip push) q1 [13 .. 100 :: Int]
+    in consume (:) [] q2
+
