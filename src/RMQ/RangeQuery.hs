8538110359a4b0991023031c481640656ff88e97
QuentinDuval <senmenty@gmail.com>
2015-12-06 15:24:17 +0100

    RMQ with just the size of the subtrees

--- src/RMQ/RangeQuery.hs
+++ src/RMQ/RangeQuery.hs
@@ -12,28 +12,33 @@ import Data.Monoid
 data RMQ a
     = Leaf
     | Node {
-        curr, mid, next :: Int,
+        treeSize        :: Int,
         nodeVal         :: a,
         lhs, rhs        :: RMQ a }
     deriving (Show, Eq, Ord)
 
 rangeSize :: RMQ a -> Int
-rangeSize rmq = next rmq - curr rmq
+rangeSize Leaf = 0
+rangeSize rmq  = treeSize rmq
 
 rangeFromList :: (Monoid a) => [a] -> RMQ a
 rangeFromList vals =
     let leaves = zipWith newLeaf [0..] vals
     in balancedFold mergeTree leaves
 
---pushBack :: (Monoid a) => RMQ a -> a -> RMQ a -- May grow the array from the top
---pushBack = undefined
+--pushBack :: (Monoid a) => RMQ a -> a -> RMQ a
+--pushBack Leaf a = undefined -- newLeaf i a
+--pushBack n@Node{..} a
+--    | rangeSize lhs > rangeSize rhs = n { rhs = pushBack rhs a }
+--    | otherwise = undefined -- insert new node
 
 updateVal :: (Monoid a) => RMQ a -> Int -> a -> RMQ a
 updateVal Leaf i v  = error "Index not found in RMQ"
 updateVal n@Node{..} i v
     | isLeafVal n   = n { nodeVal = v }
     | i < mid       = mergeTree (updateVal lhs i v) rhs
-    | otherwise     = mergeTree lhs (updateVal rhs i v)
+    | otherwise     = mergeTree lhs (updateVal rhs (i - mid) v)
+    where mid = rangeSize lhs
 
 elementAt :: (Monoid a) => RMQ a -> Int -> a    -- TODO: could be made without Monoid by just following paths
 elementAt rmq i = rangeQuery rmq (i, i+1)
@@ -41,29 +46,27 @@ elementAt rmq i = rangeQuery rmq (i, i+1)
 rangeQuery :: (Monoid a) => RMQ a -> (Int, Int) -> a
 rangeQuery Leaf _ = mempty
 rangeQuery Node{..} (b, e)
-     | b <= curr && e >= next = nodeVal
-     | b <= curr && e <  mid  = rangeQuery lhs (b, e)
-     | b >= mid  && e >= next = rangeQuery rhs (b, e)
-     | otherwise              = rangeQuery lhs (b, e) <> rangeQuery rhs (b, e)
+     | b <= 0 && e >= treeSize  = nodeVal
+     | e <  mid                 = rangeQuery lhs (b, e)
+     | b >= mid                 = rangeQuery rhs (b - mid, e - mid)
+     | otherwise                = rangeQuery lhs (b, e) <> rangeQuery rhs (b - mid, e - mid)
+     where mid = rangeSize lhs
 
 
 -- | Private
 
 newLeaf :: Int -> a -> RMQ a
-newLeaf i v = Node i i (i+1) v Leaf Leaf
+newLeaf i v = Node 1 v Leaf Leaf
 
 isLeafVal :: RMQ a -> Bool
-isLeafVal Leaf      = False
-isLeafVal Node{..}  = curr + 1 == next
+isLeafVal rmq = 1 == treeSize rmq
 
 mergeTree :: (Monoid a) => RMQ a -> RMQ a -> RMQ a
 mergeTree lhs Leaf = lhs
 mergeTree Leaf rhs = rhs
 mergeTree lhs rhs = Node {
-        curr = curr lhs,
-        mid  = curr rhs,
-        next = next rhs,
-        nodeVal = nodeVal lhs <> nodeVal rhs,
+        treeSize = treeSize lhs + treeSize rhs,
+        nodeVal  = nodeVal lhs <> nodeVal rhs,
         lhs = lhs,
         rhs = rhs
     }
