bb6b6f5f587d92ce025744e548ca4baadbbf98e7
QuentinDuval <senmenty@gmail.com>
2015-12-13 22:37:13 +0100

    Many tries, many fails

--- src/Graph/DFS.hs
+++ src/Graph/DFS.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ConstraintKinds #-}
 module Graph.DFS where
 
 import Control.Arrow
@@ -9,12 +14,26 @@ import Graph.Class
 
 -- | Lazy depth first search
 
--- TODO: using type classes, dispatch the constraints toward different algorithm (ord, or eq)
+-- Idea: Dispatch the constraints toward different algorithm (ord, or eq)
+-- This is hardly possible:
+-- You can propose different algorithm via classes, with Constraints that differ (ConstraintsKind)
+-- But it gets really hard to do some kind of fallback: try hash if you can, or ord otherwise. It requires SFINAE.
 
-dfsFrom :: (ImplicitGraph graphT nodeT edgeT, Ord nodeT) => graphT -> nodeT -> [nodeT]
+type OrdGraph graphT nodeT edgeT = (ImplicitGraph graphT nodeT edgeT, Ord nodeT)
+
+--class DFS graphtT nodeT c | nodeT -> c where
+--    dfsFrom :: (ImplicitGraph graphT nodeT edgeT, c) => graphT -> nodeT -> [nodeT]
+--
+--instance DFS graphT nodeT (Ord nodeT) where
+--    dfsFrom g s = evalState (dfsImpl g s) S.empty
+
+--instance DFS graphT nodeT (Eq nodeT) where
+--    dfsFrom g s = evalState (dfsImpl g s) S.empty
+
+dfsFrom :: (OrdGraph graphT nodeT edgeT) => graphT -> nodeT -> [nodeT]
 dfsFrom g s = evalState (dfsImpl g s) S.empty
 
-dfsImpl :: (ImplicitGraph graphT nodeT edgeT, Ord nodeT) => graphT -> nodeT -> State (S.Set nodeT) [nodeT]
+dfsImpl :: (OrdGraph graphT nodeT edgeT) => graphT -> nodeT -> State (S.Set nodeT) [nodeT]
 dfsImpl g s =
     do  modify (S.insert s)
         (s:) <$> recur (adjNodes g s)
