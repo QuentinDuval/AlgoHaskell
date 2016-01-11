1a536d90bc3f6b7ba763860a2ea3c3c7a23d76e6
QuentinDuval <senmenty@gmail.com>
2016-01-11 22:37:10 +0100

    Improve rose tree tests

--- test/Test/RoseTreeTest.hs
+++ test/Test/RoseTreeTest.hs
@@ -14,23 +14,29 @@ example = RoseTree 1 [
     RoseTree 8 []
   ]
 
+resultTree :: RoseTree Int
+resultTree = RoseTree 1 [
+    RoseTree 2 [RoseTree 2 [], RoseTree 5 []],
+    RoseTree 6 [RoseTree 8 [], RoseTree 7 []],
+    RoseTree 8 []
+  ]
+
 runRoseTreeTests :: Test
 runRoseTreeTests = TestCase $ do
 
   let z1 = zipper example
   assertEqual "Root" 1 (deref z1)
 
-  let z2 = firstChild z1
-  assertEqual "Root.Left" 2 (deref z2)
+  let z2 = firstChild (firstChild z1)
+  assertEqual "Root.Left.Left" 4 (deref z2)
 
-  let z3 = rightSibling z2
-  assertEqual "Root.Midd" 3 (deref z3)
+  let z3 = father (update z2 (`div` 2))
+  assertEqual "Root.Left" 2 (deref z3)
 
-  let z4 = firstChild z3
-  assertEqual "Root.Midd.Child" 6 (deref z4)
+  let z4 = update (rightSibling z3) (*2)
+  assertEqual "Root.Midd" 6 (deref z4)
 
-  let z5 = update z4 (* 2)
-  assertEqual "Root.Midd.Child" 12 (deref z5)
+  let z5 = update (firstChild z4) (+2)
+  assertEqual "Root.Midd.Child" 8 (deref z5)
 
-  let newExample = unzipper z5
-  assertEqual "New example" False (example == newExample)
+  assertEqual "Root.Result" resultTree (unzipper z5)
