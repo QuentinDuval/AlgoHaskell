db904b8ac30f56842bb05acaf1b0de6093e72b0c
QuentinDuval <senmenty@gmail.com>
2016-02-17 15:48:08 +0100

    First version of buildBalanced

--- src/Tree/BinaryTree.hs
+++ src/Tree/BinaryTree.hs
@@ -25,20 +25,12 @@ node = Node
 data Nat t = Zero | One t | Two t t
 
 buildBalanced :: [a] -> BinaryTree a
-buildBalanced = undefined
-
--- TODO: The merge is complex since it needs more values to be performed
-
--- buildBalanced = go []
---   where
---     go :: [Nat (BinaryTree a)] -> [a] -> BinaryTree a
---     go []  (t:ts) = go [One $ leaf t] ts
---     go acc []     = undefined
-
-    -- push :: [Nat (BinaryTree a)] -> [a] -> [Nat (BinaryTree a)]
-    -- push (Zero : cs) (v:vs)       = push (One   (leaf v) : cs) vs
-    -- push (One c: cs) (v:vs)       = push (Two c (leaf v) : cs) vs
-    -- push (Two c1 c2 : cs) (v:vs)  = Zero : push (One (node v c1 c2) : cs) vs
+buildBalanced [] = Leaf
+buildBalanced vals =
+    let len = length vals
+        mid = div (len - 1) 2
+        (left, root : right) = splitAt mid vals
+    in node root (buildBalanced left) (buildBalanced right)
 
 
 -- | Accessors
