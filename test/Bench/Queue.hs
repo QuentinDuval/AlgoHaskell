09c7fb04f4b6f6f087d6dc7e13a43d5458ac77ed
QuentinDuval <senmenty@gmail.com>
2016-02-18 13:08:07 +0100

    Test refactoring

--- test/Bench/Queue.hs
+++ test/Bench/Queue.hs
@@ -12,37 +12,39 @@ import qualified Queue.Transient as Transient
 import Queue.Utils
 
 
+-- | Run all queue benchmarks
+
 runQueueBench :: IO ()
-runQueueBench =
-    defaultMain [
-        bgroup "Queue" [
-            bgroup "NoPersist" [
-                bench "Transient"  $ nf (testNoPersist 1000)  Transient.create  ,
-                bench "RealTime"   $ nf (testNoPersist 1000)  RealTime.create   ,
-                bench "Persistent" $ nf (testNoPersist 1000)  Persistent.create ,
-                bench "Transient"  $ nf (testNoPersist 10000) Transient.create  ,
-                bench "RealTime"   $ nf (testNoPersist 10000) RealTime.create  ,
-                bench "Persistent" $ nf (testNoPersist 10000) Persistent.create ]
-            ,
-            bgroup "WithPersist" [
-                bench "Transient"  $ nf (testWithPersist 1000)  Transient.create  ,
-                bench "RealTime"   $ nf (testWithPersist 1000)  RealTime.create   ,
-                bench "Persistent" $ nf (testWithPersist 1000)  Persistent.create ,
-                bench "Transient"  $ nf (testWithPersist 10000) Transient.create  ,
-                bench "RealTime"   $ nf (testWithPersist 10000)  RealTime.create  ,
-                bench "Persistent" $ nf (testWithPersist 10000) Persistent.create ]
-            ]
-        ]
+runQueueBench = defaultMain [
+  runOneBench 1000  ,
+  runOneBench 10000 ]
+
+runOneBench :: Int -> Benchmark
+runOneBench n =
+  let name = "Queue_" ++ show n
+  in bgroup name [
+      bgroup "NoPersist" [
+          bench "Transient"  $ nf (testNoPersist n)  Transient.create  ,
+          bench "RealTime"   $ nf (testNoPersist n)  RealTime.create   ,
+          bench "Persistent" $ nf (testNoPersist n)  Persistent.create ]
+      ,
+      bgroup "WithPersist" [
+          bench "Transient"  $ nf (testWithPersist n)  Transient.create  ,
+          bench "RealTime"   $ nf (testWithPersist n)  RealTime.create   ,
+          bench "Persistent" $ nf (testWithPersist n)  Persistent.create ]
+      ]
 
 
+-- ^ Operation count: 3 * n
 testNoPersist :: (IQueue q) => Int -> ([Int] -> q Int) -> Int
 testNoPersist n create =
-    let q1 = create [0..100 :: Int]
-        q2 = foldl (flip push) q1 [100 .. 100 + n]
+    let q1 = create [1 :: Int]
+        q2 = foldl (flip push) q1 [1 .. n]
         q3 = iterate (pop . push 1) q2 !! n
     in top q3
 
 
+-- ^ Operation count: 201 * n
 testWithPersist :: (IQueue q) => Int -> ([Int] -> q Int) -> Int
 testWithPersist n create =
     let q1 = create [1 :: Int]
