39428b86e224efd8e68e9902207bd63c65edce5a
QuentinDuval <senmenty@gmail.com>
2016-02-18 12:09:42 +0100

    More monoids

--- src/Utils/Monoids.hs
+++ src/Utils/Monoids.hs
@@ -1,21 +1,33 @@
 module Utils.Monoids where
 
 
-
 -- | Minimum value monoid
 
 data Min
-    = Min { intVal :: Int }
-    | UndefMin
-    deriving (Show, Eq, Ord)
+  = Min { getMin :: Int }
+  | UndefMin
+  deriving (Show, Eq, Ord)
 
 instance Monoid Min where
-    mempty             = UndefMin
-    mappend a UndefMin = a
-    mappend UndefMin b = b
-    mappend a b        = Min $ min (intVal a) (intVal b)
+  mempty             = UndefMin
+  mappend a UndefMin = a
+  mappend UndefMin b = b
+  mappend a b        = Min $ min (getMin a) (getMin b)
 
 instance Enum Min where
-    toEnum      = Min
-    fromEnum    = intVal
+  toEnum      = Min
+  fromEnum    = getMin
 
+
+-- | Maximum value monoid
+
+data Max
+  = Max { getMax :: Int }
+  | UndefMax
+  deriving (Show, Eq, Ord)
+
+instance Monoid Max where
+  mempty             = UndefMax
+  mappend a UndefMax = a
+  mappend UndefMax b = b
+  mappend a b        = Max $ max (getMax a) (getMax b)
