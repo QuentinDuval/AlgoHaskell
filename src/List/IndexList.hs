add56a89487107fb89553c402b73cf68880a790a
QuentinDuval <senmenty@gmail.com>
2016-02-19 00:04:30 +0100

    Overall enhancement of Indexed lists
    - Add newtype instead of an alias
    - Add functor instance
    - Add foldable instance
    - Add unit tests

--- src/List/IndexList.hs
+++ src/List/IndexList.hs
@@ -6,7 +6,6 @@ module List.IndexList (
   pushFront,
   popFront,
   getFirst,
-  getLength,
   at,
   updateAt,
 ) where
@@ -28,6 +27,14 @@ treeSize n@Node{} = nodeSize n
 linkNodes :: Tree a -> Tree a -> Tree a
 linkNodes l r = Node (treeSize l + treeSize r) l r
 
+instance Functor Tree where
+  fmap fct (Leaf v) = Leaf (fct v)
+  fmap fct Node{..} = Node nodeSize (fmap fct lhs) (fmap fct rhs)
+
+instance Foldable Tree where
+  foldr fct prev (Leaf v) = fct v prev
+  foldr fct prev Node{..} = foldr fct (foldr fct prev rhs) lhs
+
 
 -- ^ Binary counter on trees, with two for efficiency
 
@@ -38,9 +45,19 @@ data DigitTree a
   deriving (Show, Eq, Ord)
 
 digitSize :: DigitTree a -> Int
-digitSize Zero = 0
-digitSize (One t) = treeSize t
+digitSize Zero      = 0
+digitSize (One t)   = treeSize t
 digitSize (Two f s) = treeSize f + treeSize s
+
+instance Functor DigitTree where
+  fmap fct Zero       = Zero
+  fmap fct (One f)    = One (fmap fct f)
+  fmap fct (Two f s)  = Two (fmap fct f) (fmap fct s)
+
+instance Foldable DigitTree where
+  foldr fct prev Zero      = prev
+  foldr fct prev (One f)   = foldr fct prev f
+  foldr fct prev (Two f s) = foldr fct (foldr fct prev s) f
 
 
 -- | The index list implementation
@@ -48,59 +65,77 @@ digitSize (Two f s) = treeSize f + treeSize s
 -- TODO: Based on this implementation:
 -- * Provide a vector like data structure (with push back)
 -- * Find a way to efficiently merge the lists
+-- * Make it traversable
 -- TODO: eliminate redundancy in 'at' and 'tree lookups'
--- TODO: make it foldable and traversable
 
-type IndexedList a = [DigitTree a]
+type Digits a = [DigitTree a]
+
+newtype IndexedList a = IndexedList {
+  digits :: Digits a
+} deriving (Show, Eq, Ord)
 
 
 -- | Public
 
 empty :: IndexedList a
-empty = []
+empty = IndexedList []
 
 fromList :: [a] -> IndexedList a
 fromList = foldr pushFront empty
 
 pushFront :: a -> IndexedList a -> IndexedList a
-pushFront a = pushTree (Leaf a)
+pushFront a = IndexedList . pushTree (Leaf a) . digits
 
 popFront :: IndexedList a -> IndexedList a
-popFront = snd . popTree
+popFront = IndexedList . snd . popTree . digits
 
 getFirst :: IndexedList a -> a
-getFirst = leafVal . fst . popTree
-
-getLength :: IndexedList a -> Int
-getLength = sum . fmap digitSize
+getFirst = leafVal . fst . popTree . digits
 
 at :: IndexedList a -> Int -> a
-at [] _ = indexError
-at (d:ds) i
-  | i >= dSize           = at ds (i - dSize)
+at = lookupList . digits
+
+updateAt :: (a -> a) -> IndexedList a -> Int -> IndexedList a
+updateAt f l = IndexedList . updateList f (digits l)
+
+
+-- | Useful instances
+
+instance Functor IndexedList where
+  fmap fct = IndexedList . fmap (fmap fct) . digits
+
+instance Foldable IndexedList where
+  length = sum . fmap digitSize . digits
+  foldr fct prev l = foldr foldDigits prev (digits l)
+    where foldDigits = flip (foldr fct)
+
+
+-- | Private (indexation)
+
+indexError = error "Error: index out of bound"
+
+lookupList :: Digits a -> Int -> a
+lookupList [] _ = indexError
+lookupList (d:ds) i
+  | i >= dSize           = lookupList ds (i - dSize)
   | i < treeSize (one d) = lookupNode (one d) i
   | otherwise            = lookupNode (two d) i
   where dSize = digitSize d
 
-updateAt :: (a -> a) -> IndexedList a -> Int -> IndexedList a
-updateAt _ [] _ = indexError
-updateAt f (d:ds) i
-  | i >= dSize           = d : updateAt f ds (i - dSize)
+lookupNode :: Tree a -> Int -> a
+lookupNode Leaf{..} _ = leafVal
+lookupNode Node{..} i
+  | i < lSize = lookupNode lhs i
+  | otherwise = lookupNode rhs (i - lSize)
+  where lSize = treeSize lhs
+
+updateList :: (a -> a) -> Digits a -> Int -> Digits a
+updateList _ [] _ = indexError
+updateList f (d:ds) i
+  | i >= dSize           = d : updateList f ds (i - dSize)
   | i < treeSize (one d) = d { one = updateNode f (one d) i } : ds
   | otherwise            = d { two = updateNode f (two d) i } : ds
   where dSize = digitSize d
-
-
--- | Private
-
-indexError = error "Error: index out of bound"
-
-lookupNode :: Tree a -> Int -> a
-lookupNode Leaf{..} _ = leafVal
-lookupTree Node{..} i
-  | i < lSize = lookupTree lhs i
-  | otherwise = lookupTree rhs (i - lSize)
-  where lSize = treeSize lhs
 
 updateNode :: (a -> a) -> Tree a -> Int -> Tree a
 updateNode f Leaf{..}   _ = Leaf (f leafVal)
@@ -109,13 +144,16 @@ updateNode f n@Node{..} i
   | otherwise = n { rhs = updateNode f rhs (i - lSize) }
   where lSize = treeSize lhs
 
-pushTree :: Tree a -> IndexedList a -> IndexedList a
+
+-- | Private (list operations)
+
+pushTree :: Tree a -> Digits a -> Digits a
 pushTree t []             = [One t]
 pushTree t (Zero : ts)    = One t : ts
 pushTree t (One f : ts)   = Two t f : ts
 pushTree t (Two f s : ts) = One t : pushTree (linkNodes f s) ts
 
-popTree :: IndexedList a -> (Tree a, IndexedList a)
+popTree :: Digits a -> (Tree a, Digits a)
 popTree [] = error "Error (popTree): list is empty"
 popTree [One f]           = (f, [])
 popTree (One f : ts)      = (f, Zero : ts)
--- test/Test/List/IndexListTest.hs
+++ test/Test/List/IndexListTest.hs
@@ -2,7 +2,7 @@ module Test.List.IndexListTest (
   runIndexListTests,
 ) where
 
-
+import Data.Foldable
 import List.IndexList
 import Test.HUnit
 
@@ -10,10 +10,28 @@ import Test.HUnit
 
 runIndexListTests :: Test
 runIndexListTests = TestList
-    [ indexAccessTest, indexUpdateTest ]
+    [ pushPopTest, foldableTest, indexAccessTest, indexUpdateTest ]
 
 
 -- | Test cases
+
+pushPopTest :: Test
+pushPopTest =
+  let l1 = fromList [0 .. 100 :: Int]
+      l2 = popFront $ popFront l1
+  in TestCase $ do
+    assertEqual "Old Head" 0 $ getFirst l1
+    assertEqual "New Head" 2 $ getFirst l2
+
+
+foldableTest :: Test
+foldableTest =
+  let l = fromList [0 .. 100 :: Int]
+  in TestCase $ do
+    assertEqual "Length"  101      $ length l
+    assertEqual "To List" [0..100] $ toList l
+    assertEqual "Functor" [1..101] $ toList (fmap (+1) l)
+    assertEqual "Sum all" 5050     $ sum l
 
 indexAccessTest :: Test
 indexAccessTest =
