29245ad5efbc888746c3968c2eb404640cb68b7b
QuentinDuval <senmenty@gmail.com>
2016-02-18 17:41:23 +0100

    Benches

--- test/Bench/Queue.hs
+++ test/Bench/Queue.hs
@@ -28,7 +28,7 @@ runNoPersist n = bgroup ("NoPersist_" ++ show n) [
     bench "Transient"  $ nf (testNoPersist n)  Transient.create  ,
     bench "RealTime"   $ nf (testNoPersist n)  RealTime.create   ,
     bench "Persistent" $ nf (testNoPersist n)  Persistent.create ,
-    --bench "DataSeq"    $ nf (testNoPersist n)  DataSeq.create    ,
+    bench "DataSeq"    $ nf (testNoPersist n)  DataSeq.create    ,
     bench "Stack"      $ nf (testNoPersist n)  Stack.create      ]
 
 runWithPersist :: Int -> Benchmark
@@ -36,7 +36,7 @@ runWithPersist n = bgroup ("WithPersist_" ++ show n) [
     bench "Transient"  $ nf (testWithPersist n)  Transient.create  ,
     bench "RealTime"   $ nf (testWithPersist n)  RealTime.create   ,
     bench "Persistent" $ nf (testWithPersist n)  Persistent.create ,
-    --bench "DataSeq"    $ nf (testWithPersist n)  DataSeq.create    ,
+    bench "DataSeq"    $ nf (testWithPersist n)  DataSeq.create    ,
     bench "Stack"      $ nf (testWithPersist n)  Stack.create      ]
 
 
@@ -49,10 +49,10 @@ testNoPersist n create =
     in consume (+) 0 q3
 
 
--- ^ Operation count: 202 * n
+-- ^ Operation count: 22 * n
 testWithPersist :: (IQueue q) => Int -> ([Int] -> q Int) -> Int
 testWithPersist n create =
     let q1 = create [1 :: Int]
         q2 = scanl (flip push) q1 [1 .. n]
-        qs = fmap ((!! 100) . iterate (pop . push 1)) q2
+        qs = fmap ((!! 10) . iterate (pop . push 1)) q2
     in sum $ fmap top qs
