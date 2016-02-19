68deb5daaadf88b8cfbce166d17c07c728196725
QuentinDuval <senmenty@gmail.com>
2016-02-19 22:23:21 +0100

    Memoization

---
+++ src/Puzzles/Memoization.hs
@@ -0,0 +1,49 @@
+module Puzzles.Memoization where
+
+import Data.Function (fix)
+import Data.IntTrie
+
+
+{-
+Memoization in Haskell:
+http://stackoverflow.com/questions/3208258/memoization-in-haskell
+-}
+
+
+-- | The function to memoize
+
+fun :: Int -> Integer
+fun n
+  | n < 1 = 1
+  | otherwise = max (fromIntegral n) (fun (div n 2) + fun (div n 3) + fun (div n 4))
+
+
+-- | A pure version supporting memoization
+-- Makes use of open recursion
+
+fun' :: (Int -> Integer) -> Int -> Integer
+fun' f n
+  | n < 1 = 1
+  | otherwise = max (fromIntegral n) (f (div n 2) + f (div n 3) + f (div n 4))
+
+
+-- | Memoization via an infinite list
+-- Good lazyness but bad random access
+-- If you remove the argument of memoList, memoization stays between function calls
+
+memoList :: Int -> Integer
+memoList n = memoized n
+  where
+    memoized  = fun' (memoTable !!)
+    memoTable = fmap memoized [0..n]
+
+
+-- | Memoization via an infinite int-trie
+-- Good lazyness and log N random access
+-- If you remove the argument of memoList, memoization stays between function calls
+
+memoTree :: Int -> Integer
+memoTree n = memoized n
+  where
+    memoized  = fun' (memoTable `apply`)
+    memoTable = fmap memoized identity
