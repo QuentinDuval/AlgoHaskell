2ed00a7980592fdc5b264865e1fa0e8f7f25954b
QuentinDuval <senmenty@gmail.com>
2016-02-19 19:40:09 +0100

    Minor change in the histogram

--- src/Puzzles/Histogram.hs
+++ src/Puzzles/Histogram.hs
@@ -15,9 +15,9 @@ http://www.seas.upenn.edu/~cis194/spring13/hw/03-rec-poly.pdf
 
 horizontalBars :: [Int] -> [String]
 horizontalBars vals =
-  let len = maximum vals
-      bar n = replicate n '*' ++ repeat ' '
-  in fmap (take len . bar) vals
+  let totalLen = maximum vals
+      horBar n = replicate n '*' ++ repeat ' '
+  in fmap (take totalLen . horBar) vals
 
 toVerticalBars :: [String] -> [String]
 toVerticalBars = fmap reverse >>> transpose
@@ -25,18 +25,18 @@ toVerticalBars = fmap reverse >>> transpose
 horizontalLegends :: (Int, Int) -> [String]
 horizontalLegends (start, end) = padL lenSize <$> legends
   where
-    legends = show <$> [start..end]
-    lenSize = maximum (length <$> legends)
+    legends = show <$> [start .. end]
+    lenSize = maximum (fmap length legends)
 
 verticalLegends :: (Int, Int) -> [String]
 verticalLegends r@(start, end) = sepLine : transpose (horizontalLegends r)
   where sepLine = replicate (end - start + 1) '='
 
 showHistogram :: (Int, Int) -> [Int] -> String
-showHistogram r =
+showHistogram range =
   horizontalBars
     >>> toVerticalBars
-    >>> (++ verticalLegends r)
+    >>> (++ verticalLegends range)
     >>> intercalate "\n"
 
 
@@ -47,11 +47,11 @@ frequencies increment = foldl (\m k -> M.insertWith (+) k increment m) M.empty
 
 frequenciesIn :: (Int, Int) -> [Int] -> [Int]
 frequenciesIn (start, end) =
-  let r = [start..end]
-  in filter (`elem` r)                        -- Remove elements out of the range
-      >>> frequencies 1                       -- Count the number of each element
-      >>> M.unionWith (+) (frequencies 0 r)   -- Add missing elements with count 0
-      >>> fmap snd . M.toAscList              -- Return the counts
+  let range = [start..end]
+  in filter (`elem` range)                      -- Remove elements out of the range
+      >>> frequencies 1                         -- Count the number of each element
+      >>> M.unionWith (+) (frequencies 0 range) -- Add missing elements with count 0
+      >>> fmap snd . M.toAscList                -- Return the counts
 
 
 -- | Assembling the pieces together
@@ -65,5 +65,7 @@ histogram r = frequenciesIn r >>> showHistogram r
 test :: IO ()
 test = do
     let input = [11,1,4,5,4,6,6,3,4,2,4,9,11,-1]
+    putStrLn ""
     putStrLn $ histogram (0,9) input
+    putStrLn ""
     putStrLn $ histogram (-1,11) input
