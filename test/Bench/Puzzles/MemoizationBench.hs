7cb0e83774072405645e86716b0ece8a80ffd100
QuentinDuval <senmenty@gmail.com>
2016-02-20 20:14:28 +0100

    Enhancement of the benchmarks

--- test/Bench/Puzzles/MemoizationBench.hs
+++ test/Bench/Puzzles/MemoizationBench.hs
@@ -14,7 +14,28 @@ runMemoBench =
   let inputs = V.fromList [1 .. 1000]
   in defaultMain [
         bgroup "Memoize" [
-            bench "memoCutRod"      $ nf memoCutRod     inputs,
-            bench "memoCutRod2"     $ nf memoCutRod2    inputs
+            cutRodBench,
+            memoSparseBench
         ]
       ]
+
+
+cutRodBench :: Benchmark
+cutRodBench =
+  let inputs = V.fromList [1 .. 1000]
+  in bgroup "CutRod" [
+        bench "memoCutRod"      $ nf memoCutRod     inputs,
+        bench "memoCutRod2"     $ nf memoCutRod2    inputs
+     ]
+
+memoSparseBench :: Benchmark
+memoSparseBench =
+ let inputs = 1000000
+ in bgroup "MemoSparse" [
+       bench "memoList"      $ nf memoList     inputs,
+       bench "memoVector"    $ nf memoVector   inputs,
+       bench "memoTree"      $ nf memoTree     inputs,
+       bench "memoLazyMap"   $ nf memoMap      inputs,
+       bench "memoModule"    $ nf memoModule   inputs,
+       bench "memoMonadMap"  $ nf memoMonad    inputs
+    ]
