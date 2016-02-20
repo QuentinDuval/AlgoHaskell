f53050e3c28fc14425b8ab81d0f690124bfb6f8b
QuentinDuval <senmenty@gmail.com>
2016-02-20 23:56:48 +0100

    Unit tests on Knapsack

--- test/Test/Puzzles/MemoizationTest.hs
+++ test/Test/Puzzles/MemoizationTest.hs
@@ -11,14 +11,30 @@ import Test.HUnit
 
 runAllMemoTests :: Test
 runAllMemoTests = TestList [
-        runMemoTest "memoCutRod"  memoCutRod,
-        runMemoTest "memoCutRod2" memoCutRod2
+        runCutRodTest   "memoCutRod"  memoCutRod,
+        runCutRodTest   "memoCutRod2" memoCutRod2,
+        runKnapsackTest "naiveKnapsack" naiveKnapsack,
+        runKnapsackTest "memoKnapsack"  memoKnapsack
     ]
 
 
-runMemoTest :: String -> (V.Vector Int -> Int) -> Test
-runMemoTest str cutRodImpl =
+runCutRodTest :: String -> (V.Vector Int -> Int) -> Test
+runCutRodTest str cutRodImpl =
     TestCase $ do
       assertEqual str 100 (cutRodImpl $ V.fromList [1..100])
       assertEqual str 100 (cutRodImpl $ V.fromList $ replicate 100 1)
       assertEqual str 10  (cutRodImpl $ V.fromList [1, 5, 8, 9])
+
+
+runKnapsackTest :: String -> (V.Vector Item -> Int -> Int) -> Test
+runKnapsackTest str algoImpl =
+  let items = V.fromList [(1, 1), (2, 3), (5, 5)]
+  in TestCase $ do
+    assertEqual str 0 (algoImpl items 0)
+    assertEqual str 1 (algoImpl items 1)
+    assertEqual str 3 (algoImpl items 2)
+    assertEqual str 4 (algoImpl items 3)
+    assertEqual str 5 (algoImpl items 5)
+    assertEqual str 6 (algoImpl items 6)
+    assertEqual str 8 (algoImpl items 7)
+    assertEqual str 9 (algoImpl items 8)
